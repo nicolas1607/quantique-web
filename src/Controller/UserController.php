8c78c5bd6bdb52e4205336f1075bf6ef14be8d9d
Nicolas Mormiche <nicolas160796@gmail.com>
2021-11-16 13:28:07 +0100

    [ADMIN] Création du listing des stats, contrats, factures

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Entity\GoogleAccount;
+use App\Entity\FacebookAccount;
 use App\Repository\UserRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserController extends AbstractController
 {
+    private EntityManagerInterface $em;
     private UserRepository $userRepo;
 
-    public function __construct(UserRepository $userRepo)
+    public function __construct(EntityManagerInterface $em, UserRepository $userRepo)
     {
+        $this->em = $em;
         $this->userRepo = $userRepo;
     }
 
@@ -33,7 +42,7 @@ class UserController extends AbstractController
                 $users[] = $user;
             }
         }
-        return $this->render('admin/admin.html.twig', [
+        return $this->render('admin/user.html.twig', [
             'users' => $users
         ]);
     }
@@ -45,4 +54,115 @@ class UserController extends AbstractController
     {
         return $this->render('user/profile.html.twig', []);
     }
+
+    /**
+     * @Route("/user/modify/{id}", name="modify_user")
+     */
+    public function modifyUser(Request $request, User $user, UserPasswordHasherInterface $encoder): Response
+    {
+        $modifyUserForm = $this->createForm(UserType::class, $user, ['method' => 'GET']);
+
+        $modifyUserForm->handleRequest($request);
+
+        if ($modifyUserForm->isSubmitted() && $modifyUserForm->isValid()) {
+            $user = $modifyUserForm->getData();
+
+            // On créer les comptes Google & Facebook
+            $emailGoogle = $request->query->get('emailGoogle');
+            $mdpGoogle = $request->query->get('mdpGoogle');
+            if ($emailGoogle && $mdpGoogle) {
+                $accountGoogle = new GoogleAccount();
+                $accountGoogle->setEmail($emailGoogle)
+                    ->setPassword($encoder->hashPassword($user, $mdpGoogle));
+                $user->setGoogleAccount($accountGoogle);
+                $this->em->persist($accountGoogle);
+            }
+            $emailFb = $request->query->get('emailFb');
+            $mdpFb = $request->query->get('mdpFb');
+            if ($emailFb && $mdpFb) {
+                $accountFb = new FacebookAccount();
+                $accountFb->setEmail($emailFb)
+                    ->setPassword($encoder->hashPassword($user, $mdpFb));
+                $user->setGoogleFacebook($accountFb);
+                $this->em->persist($accountFb);
+            }
+
+            $password = $user->getPassword();
+            $passwordEncoded = $encoder->hashPassword($user, $password);
+            $user->setPassword($passwordEncoded);
+
+            $this->em->persist($user);
+            $this->em->flush();
+
+            return $this->redirectToRoute('user');
+        }
+
+        return $this->render('user/modify.html.twig', [
+            'modify_user_form' => $modifyUserForm->createView(),
+            'user' => $user
+        ]);
+    }
+
+
+    /**
+     * @Route("/user/stats/{id}", name="show_stats_user")
+     */
+    public function showStats(User $user): Response
+    {
+        return $this->render('user/show_stats.html.twig', [
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * @Route("/user/contracts/{id}", name="show_contracts_user")
+     */
+    public function showContracts(User $user): Response
+    {
+        return $this->render('user/show_contracts.html.twig', [
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * @Route("/user/invoices/{id}", name="show_invoices_user")
+     */
+    public function showInvoices(Request $request, User $user): Response
+    {
+        $type = $request->query->get('typeContract');
+        if ($type == null) $type = 'all';
+
+        $res = [];
+        foreach ($user->getContracts() as $contract) {
+            foreach ($contract->getInvoices() as $invoice) {
+                if ($type != 'all') {
+                    if ($invoice->getContract()->getType()->getLib() == $type) {
+                        $res[] = $invoice;
+                    }
+                } else {
+                    $res[] = $invoice;
+                }
+            }
+        }
+        usort($res, function ($a, $b) {
+            return $a < $b ? -1 : 1;
+        });
+
+        return $this->render('user/show_invoices.html.twig', [
+            'user' => $user,
+            'invoices' => $res,
+            'type' => $type
+        ]);
+    }
+
+    /**
+     * @Route("/user/add/contract/{id}", name="add_contract")
+     */
+    // public function addContract(User $user): Response
+    // {
+
+    //     return $this->render('user/s_contracts.html.twig', [
+    //         'user' => $user
+    //     ]);
+    // }
 }
---
+++ src/Form/UserType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\GoogleAccount;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', TextType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('contact', TextType::class, [
+                'label' => 'Contact',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer',
+                'attr' => [
+                    'class' => 'btn btn-type'
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
